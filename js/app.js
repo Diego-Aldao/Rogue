27d0dc18ccf8d825cac869219e1a3dd48b40e635
Diego97-webdev <shizusenpai97@gmail.com>
2020-07-03 21:23:16 -0300

    agregada la seccion de compra mujeres/hombres y actualizada la version del swiper

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 var swiper = new Swiper(".swiper-container", {
   spaceBetween: 30,
   effect: "fade",
-  speed: 2000,
+  speed: 1000,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".contenedor-slider", {
+  slidesPerView: 2,
+  spaceBetween: 10,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
