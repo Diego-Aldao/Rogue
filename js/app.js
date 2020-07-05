2980b02c079e1b763169de1be77740c6db703b15
Diego97-webdev <shizusenpai97@gmail.com>
2020-07-05 20:09:22 -0300

    agregadas las animaciones del header con gsap

--- js/app.js
+++ js/app.js
@@ -29,3 +29,29 @@ var swiper = new Swiper(".contenedor-slider-productos-categorias", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//ANIMACIONES CON GSAP
+
+gsap.from(".titulos-principales", {
+  duration: 1,
+  x: -150,
+  opacity: 0,
+  delay: 0.2,
+});
+gsap.from(".btn-principal", {
+  duration: 1,
+  y: 100,
+  opacity: 0,
+  delay: 0.7,
+});
+gsap.from(".mini-texto", {
+  duration: 1.6,
+  opacity: 0,
+  delay: 1,
+  delay: 0.6,
+});
+
+gsap.from(".body", {
+  duration: 2,
+  opacity: 0,
+});
