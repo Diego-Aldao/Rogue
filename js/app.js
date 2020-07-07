eef07729f53f37ba1b0c4dbc65489ec8dc1a7f88
Diego97-webdev <shizusenpai97@gmail.com>
2020-07-07 18:11:42 -0300

    agregado el menu desplegable

--- js/app.js
+++ js/app.js
@@ -53,7 +53,6 @@ gsap.from(".btn-principal", {
 gsap.from(".mini-texto", {
   duration: 1.6,
   opacity: 0,
-  delay: 1,
   delay: 0.6,
 });
 
@@ -61,3 +60,106 @@ gsap.from(".body", {
   duration: 2,
   opacity: 0,
 });
+
+//MENU DESPLEGABLE
+
+//EVENT LISTENERS DEL MENU DESPLEGABLE
+(function () {
+  const menuDesplegable = document.querySelector(".menu-desplegable");
+  const menuHamburguesa = document.querySelector(".menu-hamburguesa");
+  const botonCerrar = document.querySelector(".btn-cerrar");
+
+  menuHamburguesa.addEventListener("click", () => {
+    gsap.to(menuDesplegable, {
+      duration: 0.5,
+      left: 0,
+    });
+  });
+
+  botonCerrar.addEventListener("click", () => {
+    gsap.to("body", {
+      opacity: 1,
+    });
+    gsap.to(menuDesplegable, {
+      duration: 0.5,
+      left: "-100vw",
+    });
+  });
+})();
+
+//EVENT LISTENERS NAV-mujeres
+(function () {
+  const mujeresNav = document.querySelector(".nav-mujer");
+  const popUpMujer = document.querySelector(".select-mujeres");
+  const menuOculto = document.querySelector(".menu-oculto");
+
+  /*mujeresNav.addEventListener("mouseover", () => {
+    gsap.to(popUpMujer, {
+      height: 50,
+      opacity: 1,
+    });
+    gsap.to(".link-mujeres", {
+      delay: 0.2,
+      marginBottom: 0,
+    });
+  });*/
+
+  /*menuOculto.onmouseover = function (e) {
+    let posicionMouse = e.target.className;
+    console.log(posicionMouse);
+    if (posicionMouse == "links nav-mujer") {
+      gsap.to(popUpMujer, {
+        height: 50,
+        opacity: 1,
+      });
+      gsap.to(".link-mujeres", {
+        delay: 0.2,
+        marginBottom: 0,
+      });
+    }
+    setTimeout(() => {
+      if (
+        posicionMouse != "link-pop-up link-mujeres" ||
+        "mj" ||
+        "links nav-mujer"
+      ) {
+        gsap.to(popUpMujer, {
+          height: 0,
+          opacity: 0,
+        });
+        gsap.to(".link-mujeres", {
+          delay: 0.2,
+          marginBottom: 50,
+        });
+      }
+    }, 1000);
+  };
+*/
+  popUpMujer.addEventListener("mouseleave", () => {});
+
+  //EVENT LISTENERS NAV-hombres
+  const hombresNav = document.querySelector(".nav-hombre");
+  const popUpHombres = document.querySelector(".select-hombres");
+  /*hombresNav.addEventListener("mouseover", () => {
+    gsap.to(popUpHombres, {
+      height: 50,
+      opacity: 1,
+    });
+    gsap.to(".link-pop-up", {
+      delay: 0.2,
+      marginBottom: 0,
+    });
+  });
+
+  hombresNav.addEventListener("mouseover", () => {
+    if (popUpMujer.style.height == "50px") {
+      gsap.to(popUpMujer, {
+        height: 0,
+        opacity: 0,
+      });
+      gsap.to(".link-mujeres", {
+        marginBottom: 150,
+      });
+    }
+  });*/
+})();
